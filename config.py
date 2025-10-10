d50d6cabc249a13a6222b0a42832a6885b703142
AkaGop <150795195+AkaGop@users.noreply.github.com>
2025-10-10 15:32:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # config.py
 
-"""
-Single source of truth for all static configuration data, knowledge bases, and report definitions.
-"""
-
 SECS_MAP = {
     "S1F1": "Are You There Request", "S1F2": "Are You There Data",
     "S1F3": "Selected Equipment Status Request", "S1F4": "Selected Equipment Status Data",
@@ -14,7 +10,7 @@ SECS_MAP = {
 CEID_MAP = {
     11: "GemEquipmentOFFLINE", 12: "GemControlStateLOCAL", 13: "GemControlStateREMOTE",
     14: "GemMsgRecognition", 16: "GemPPChangeEvent", 30: "GemProcessStateChange",
-    101: "AlarmClear", 102: "AlarmSet", 18: "AlarmSet", 113: "AlarmSet", 114: "AlarmSet",
+    101: "AlarmClear", 102: "AlarmSet", 18: "AlarmSet", 113: "AlarmSet", 114: "AlarmSet", 
     120: "IDRead", 121: "UnloadedFromMag/LoadedToTool", 127: "LoadedToTool",
     131: "LoadToToolCompleted", 132: "UnloadFromToolCompleted", 136: "MappingCompleted",
     141: "PortStatusChange", 151: "MagazineDocked", 180: "RequestMagazineDock",
